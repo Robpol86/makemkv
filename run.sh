e25585b0579460a3a40d85e16abee52827bf139b
Robpol86 <robpol86@gmail.com>
2017-03-31 22:23:09 -0700

    Handling low disk space.
    makemkvcon assumes "yes" when it prompts the user to continue due to not
    enough disk space. It never actually waits for user input even with a
    valid stdin tty. No configuration or command line option either. The
    only solution is to watch for string patterns and kill the entire bash
    script and its children (includes makemkvcon).
    
    Also makemkvcon seems to not care about broken pipes. So pipefail isn't
    enough. Must kill 0.

--- run.sh
+++ run.sh
@@ -8,5 +8,15 @@ set -e  # Exit script if a command fails.
 set -u  # Treat unset variables as errors and exit immediately.
 set -o pipefail  # Exit script if pipes fail instead of just the last program.
 
+# Kill makemkvcon when not enough disk space. It keeps going no matter what.
+catch_low_space () {
+    ret=0
+    sed "/much as [0-9]\+ megabytes while there are only/q5" -- "$@" || ret=$?
+    [ ${ret} -ne 5 ] && return
+    echo -e "\nERROR: Terminating MakeMKV due to low disk space.\n"
+    kill 0
+}
+
+
 # Rip media.
-makemkvcon mkv -r --progress -same disc:0 all /output
+makemkvcon mkv -r --progress -same disc:0 all /output |catch_low_space
