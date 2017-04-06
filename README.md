1a6ec52b0142281319b037d13e4c37b8a679bcf7
Robpol86 <robpol86@gmail.com>
2017-04-06 12:29:27 -0700

    Reverting ddrescue/ROBUST. Didn't really help.
    ddrescue took over 12 hours to run on my Matrix DVD. This is out of
    scope. Most DVDs and Blurays seem to work with just makemkvcon. And
    makemkvcon worked on that same Matrix dvd on a ThinkPad so it's probably
    my USB drive.
    
    Always creating unique subdirectory, even if re-ripping the same DVD. My
    fears of the udev rule running multiple times on a single insert were
    unfounded. Since mktemp creates unique directories no longer need to
    watch for overwrite messages in makemkvcon. Also no longer letting users
    specify dest sub directory. It's automated.
    
    Using directio on makemkvcon and reducing retries to 1 (I think the
    default is 3 or 5). Better handling of bad sector copy protection.

--- README.md
+++ README.md
@@ -12,24 +12,13 @@ or other formats. Ripped Blurays may take up around **40 GB** or so in my experi
 Below are the available environment variables you may use to configure this Docker image:
 
 * **DEBUG** Enables debug output if set to "true".
-* **DIRECTORY** Subdirectory name to write MKVs in.
 * **MKV_GID** The group ID of the `mkv` user inside the container.
 * **MKV_UID** The user ID of the `mkv` user inside the container.
 * **NO_EJECT** Disables ejecting the disc if set to "true".
-* **ROBUST** Better handling of copy protection if set to "true".
 
 And below are the available volumes used by the Docker image:
 
 * **/output** Ripped MKV files are written to this directory inside the container.
-
-By default **DIRECTORY** is the name of the disc. If you set the variable to an empty string MKV files will be written
-to the root of the mapped **/output** directory (no subdirectories).
-
-The **ROBUST** variable is for some DVDs that include errors on the disc that MakeMKV doesn't handle well. Setting this
-to "true" introduces an intermediary step where the DVD is ripped into a still-encrypted ISO by `ddrescue` which has
-better error handling. Afterwards makemkvcon is run from that ISO to handle decrypting and creating MKV files. The
-downside to this is that you'll need twice as much free disk space available to hold the ISO and the MKV files at the
-same time.
 
 ## Run Manually
 
--- run.sh
+++ run.sh
@@ -12,7 +12,6 @@ declare -i MKV_GID=${MKV_GID:-0}
 declare -i MKV_UID=${MKV_UID:-0}
 declare -l DEBUG=${DEBUG:-} && [ "$DEBUG" == "true" ] || DEBUG=
 declare -l NO_EJECT=${NO_EJECT:-} && [ "$NO_EJECT" == "true" ] || NO_EJECT=
-declare -l ROBUST=${ROBUST:-} && [ "$ROBUST" == "true" ] || ROBUST=
 
 # Print environment.
 if [ "$DEBUG" == "true" ]; then
@@ -20,36 +19,12 @@ if [ "$DEBUG" == "true" ]; then
     env |sort
 fi
 
-# Determine destination directory.
-ID_FS_LABEL=${ID_FS_LABEL:-$(blkid -o value -s LABEL)}
-ID_FS_UUID=${ID_FS_UUID:-$(blkid -o value -s UUID)}
-DIRECTORY=/output/${DIRECTORY-${ID_FS_LABEL:-${ID_FS_UUID:-}}}
-
-# Determine device file for ddrescue and eject.
-DEVICE=
-for d in /dev/cdrom /dev/sr[0-9]*; do
-    if [ -b "$d" ]; then
-        DEVICE="$d"
-        break
-    fi
-done
-
 # Kill makemkvcon when not enough disk space. It keeps going no matter what.
 low_space_term () {
     local ret=0
     sed -u "/much as [0-9]\+ megabytes while there are only/q5" || ret=$?
     [ ${ret} -ne 5 ] && return
     echo -e "\nERROR: Terminating MakeMKV due to low disk space.\n" >&2
-    sync
-    kill 0
-}
-
-# Kill makemkvcon to prevent overwriting/re-ripping.
-no_overwrite () {
-    local ret=0
-    sed -u "/Do you want to overwrite it/q5" || ret=$?
-    [ ${ret} -ne 5 ] && return
-    echo -e "\nERROR: Terminating MakeMKV due to file already exists.\n" >&2
     sync
     kill 0
 }
@@ -72,37 +47,33 @@ if [ "$MKV_GID" -ne "0" ] && [ "$MKV_GID" -ne "$(id -g mkv)" ]; then
     groupmod -og "$MKV_GID" mkv
 fi
 
-# Create destination directory.
-if [ ! -e "$DIRECTORY" ]; then
-    sudo -u mkv mkdir -p "$DIRECTORY"
-fi
+# Determine destination directory.
+ID_FS_LABEL=${ID_FS_LABEL:-$(blkid -o value -s LABEL)}
+ID_FS_UUID=${ID_FS_UUID:-$(blkid -o value -s UUID)}
+TEMPLATE="${ID_FS_LABEL:-nolabel}_${ID_FS_UUID:-nouuid}_XXX"
+DIRECTORY=$(sudo -u mkv mktemp -d "/output/$TEMPLATE")
 
 # Rip media.
 echo "Ripping..."
-src=disc:0
-if [ "$ROBUST" == "true" ]; then
-    src="iso:$DIRECTORY/encrypted.iso"
-    sudo -u mkv ddrescue -b 2048 -d ${DEBUG:+-v} "$DEVICE" "${src:4}"
-    echo "Done ripping to ISO. Converting (lossless) to MKV..."
-fi
-sudo -u mkv makemkvcon mkv --progress -same "$src" all "$DIRECTORY" \
+sudo -u mkv makemkvcon mkv --progress -same --directio true disc:0 all "$DIRECTORY" \
     |low_space_term \
-    |no_overwrite \
     |catch_failed
 
 # Eject.
 if [ "$NO_EJECT" != "true" ]; then
     echo "Ejecting..."
-    if [ -z "$DEVICE" ]; then
+    device=
+    for d in /dev/cdrom /dev/sr[0-9]*; do
+        if [ -b "$d" ]; then
+            device="$d"
+            break
+        fi
+    done
+    if [ -z "$device" ]; then
         echo -e "\nERROR: Unable to find optical device to eject.\n" >&2
         exit 1
     fi
-    eject ${DEBUG:+--verbose} "$DEVICE"
-fi
-
-# Cleanup.
-if [ "$ROBUST" == "true" ]; then
-    sudo -u mkv rm ${DEBUG:+-v} "${src:4}"  # Delete ISO since makemkvcon finished.
+    eject ${DEBUG:+--verbose} "$device"
 fi
 
 echo "Done after $(date -u -d @$SECONDS +%T)"
