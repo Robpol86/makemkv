165d1f4df3e033d90eaae82aec21180c5626c01f
Robpol86 <robpol86@gmail.com>
2017-04-16 22:53:45 -0700

    Testing udev rule in README.
    Testing manual and udev commands straight from README in Travis. Need to
    update Docker to support -d and --rm.

--- test.sh
+++ test.sh
@@ -5,18 +5,32 @@ set -u  # Treat unset variables as errors and exit immediately.
 set -x  # Print command traces before executing command.
 set -o pipefail  # Exit script if pipes fail instead of just the last program.
 
+MKV_GLOB=/tmp/MakeMKV/Sample_2017-04-15-15-16-14-00_???/title00.mkv
+
+# Verify generated MKV file.
+verify () {
+    # Verify no extra files remain.
+    test $(find /tmp/MakeMKV -print |tee /dev/stderr |wc -l) -eq 3
+    test $(ls -l ${MKV_GLOB} |tee /dev/stderr |wc -l) -eq 1
+    local MKV=$(ls ${MKV_GLOB} |tee /dev/stderr)
+
+    # Verify mkv file stat.
+    test "$(stat -c %u ${MKV} |tee /dev/stderr)" == "$UID"
+    test "$(stat -c %g ${MKV} |tee /dev/stderr)" == "$(id -g)"
+    test "$(stat -c %a ${MKV} |tee /dev/stderr)" == 644
+    test "$(stat -c %s ${MKV} |tee /dev/stderr)" == 17345216
+
+    # Cleanup.
+    rm -r /tmp/MakeMKV; mkdir $_
+}
+
 # Run manually.
 cdemu load 0 sample.iso
-mkdir /tmp/MakeMKV
-docker run -it --device=/dev/cdrom -e MKV_GID=$(id -g) -e MKV_UID=$(id -u) -v /tmp/MakeMKV:/output robpol86/makemkv
+source <(sed -n '/^Now go ahead and run the image:$/{:a;n;/```$/b;p;ba}' README.md |tail -n +3)
+verify
 
-# Verify no extra files remain.
-test $(find /tmp/MakeMKV -print |tee /dev/stderr |wc -l) -eq 3
-test $(ls -l /tmp/MakeMKV/Sample_2017-04-15-15-16-14-00_???/title00.mkv |tee /dev/stderr |wc -l) -eq 1
-MKV=$(ls /tmp/MakeMKV/Sample_2017-04-15-15-16-14-00_???/title00.mkv |tee /dev/stderr)
-
-# Verify mkv file stat.
-test "$(stat -c %u ${MKV} |tee /dev/stderr)" == "$UID"
-test "$(stat -c %g ${MKV} |tee /dev/stderr)" == "$(id -g)"
-test "$(stat -c %a ${MKV} |tee /dev/stderr)" == 644
-test "$(stat -c %s ${MKV} |tee /dev/stderr)" == 17345216
+# Run via udev.
+sed -n '/^# Save as:.\+85-makemkv.rules/{:a;n;/```/b;p;ba}' README.md |sudo tee /etc/udev/rules.d/85-makemkv.rules
+cdemu load 0 sample.iso
+for _ in $(seq 1 20); do sleep 0.5; if stat ${MKV_GLOB} &>/dev/null; then break; fi; done
+verify
