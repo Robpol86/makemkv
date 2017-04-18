3bb5ae09d80767dec8cf2478e2172430c5335401
Robpol86 <robpol86@gmail.com>
2017-04-17 19:35:59 -0700

    Testing low space handling.
    Make sure low disk space is handled correctly in bash tests.
    
    Split up test.sh in a way to more easily keep track of which test fails.

--- test.sh
+++ test.sh
@@ -19,18 +19,46 @@ verify () {
     test "$(stat -c %g ${MKV} |tee /dev/stderr)" == "$(id -g)"
     test "$(stat -c %a ${MKV} |tee /dev/stderr)" == 644
     test "$(stat -c %s ${MKV} |tee /dev/stderr)" == 17345216
-
-    # Cleanup.
-    rm -r /tmp/MakeMKV; mkdir $_
 }
 
-# Run manually.
-cdemu load 0 sample.iso
-source <(sed -n '/^Now go ahead and run the image:$/{:a;n;/```$/b;p;ba}' README.md |tail -n +3)
-verify
+# Assert failures
+assert_failed () {
+    local ret=0
+    docker run -it --device=/dev/cdrom -v /tmp/MakeMKV:/output robpol86/makemkv 2>&1 || ret=$?
+    test ${ret} -gt 0
+}
 
-# Run via udev.
-sed -n '/^# Save as:.\+85-makemkv.rules/{:a;n;/```/b;p;ba}' README.md |sudo tee /etc/udev/rules.d/85-makemkv.rules
-cdemu load 0 sample.iso
-for _ in $(seq 1 20); do sleep 0.5; if stat ${MKV_GLOB} &>/dev/null; then break; fi; done
-verify
+# Clean before running test.
+sudo umount /tmp/MakeMKV &> /dev/null || true
+rm -rf /tmp/MakeMKV
+sudo rm -f /etc/udev/rules.d/85-makemkv.rules
+
+# Run test cases.
+case "$1" in
+    manual)
+        # Run manually from README.
+        cdemu load 0 sample.iso
+        source <(sed -n '/^Now go ahead and run the image:$/{:a;n;/```$/b;p;ba}' README.md |tail -n +3)
+        verify
+        ;;
+    udev)
+        # Run via udev from README.
+        mkdir /tmp/MakeMKV
+        sed -n '/^# Save as:.\+makemkv.rules/{:a;n;/```/b;p;ba}' README.md |sudo tee /etc/udev/rules.d/85-makemkv.rules
+        cdemu load 0 sample.iso
+        for _ in $(seq 1 20); do sleep 0.5; if stat ${MKV_GLOB} &>/dev/null; then break; fi; done
+        verify
+        ;;
+    low_space)
+        # Test low space handling.
+        mkdir /tmp/MakeMKV
+        dd if=/dev/zero of=/tmp/fs.bin bs=1M count=5
+        mkfs.ext4 -F /tmp/fs.bin
+        sudo mount -t ext4 -o loop /tmp/fs.bin /tmp/MakeMKV
+        cdemu load 0 sample.iso
+        assert_failed |grep "ERROR: Terminating MakeMKV due to low disk space."
+        ;;
+    *)
+        echo "INVALID TEST CASE"
+        exit 1
+esac
