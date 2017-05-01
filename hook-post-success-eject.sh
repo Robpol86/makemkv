31bf6bc5694e33c2502a0f9697d16846eff1263c
Robpol86 <robpol86@gmail.com>
2017-05-01 15:24:21 -0700

    Re-containerizing as mp4.
    Avoids running ffmpeg twice on errors due to two hook scripts.

--- hook-post-success-eject.sh
+++ hook-post-success-eject.sh
@@ -10,7 +10,7 @@ mkdir "$DIR_ORIGINAL" "$DIR_WORKING"
 
 for src_file in "$DIR_FINAL"/*.mkv; do
     filename=$(basename "$src_file")
-    dst_file="$DIR_WORKING/$filename"
+    dst_file="$DIR_WORKING/${filename%.*}.mp4"
     sudo -u mkv ffmpeg -i "$src_file" -map 0:0 -map 0:1 -acodec copy -vcodec copy -sn "$dst_file"
     sudo -u mkv mv "$src_file" "$DIR_ORIGINAL"
     sudo -u mkv mv "$dst_file" "$DIR_FINAL"
