9d871851c36e05869ea39b1a7fc8c457d20384b8
Tony Brobston <anthonybrobston@gmail.com>
2017-10-26 22:30:45 -0500

    Pretty sure -z is supposed to be -n, kind of new to bash, still need to run this through a full test manually

--- hook-end.sh
+++ hook-end.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ -z "$EMAIL" ] && [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+if [ -n "$EMAIL" ] && [ -n "$AWS_ACCESS_KEY_ID" ] && [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
     send_email "Rip was successful" "This is an email notification reminding you that your Rip has finished successfully. Enjoy!" &
 fi
--- hook-post-on-err.sh
+++ hook-post-on-err.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-if [ -z "$EMAIL" ] && [ -z "$AWS_ACCESS_KEY_ID" ] && [ -z "$AWS_SECRET_ACCESS_KEY" ]; then
+if [ -n "$EMAIL" ] && [ -n "$AWS_ACCESS_KEY_ID" ] && [ -n "$AWS_SECRET_ACCESS_KEY" ]; then
     send_email "Rip was unsuccessful" "This is an email notification let you know that your Rip was unsuccessful." &
 fi
