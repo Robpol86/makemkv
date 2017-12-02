32d1b7a23a38016d4c1e9decac1c559d2e16e4c4
Tony Brobston <anthonybrobston@gmail.com>
2017-12-01 18:48:26 -0600

    Handling one special character

--- bin/rip.sh
+++ bin/rip.sh
@@ -10,7 +10,7 @@ set -u  # Treat unset variables as errors and exit immediately.
 set -o pipefail  # Exit script if pipes fail instead of just the last program.
 
 # Add latest beta key.
-KEY="$(curl -s 'https://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053' | grep -oP 'T-[\w_-]{66}')";
+KEY="$(curl -s 'https://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053' | grep -oP 'T-[\@\w_-]{66}')";
 if [ -n "$KEY" ]; then
     sed -i "/app_Key/ d" /home/mkv/.MakeMKV/settings.conf
     echo -e "\nINFO: Adding new beta key.\n"
